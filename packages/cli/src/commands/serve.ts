d3f238b635dec8fec2b9860baf30b549e00df787
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2021-03-28 22:38:49 -0300

    restore solution files

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,5 +1,5 @@
-import { Command } from "commander";
 import path from "path";
+import { Command } from "commander";
 import { serve } from "@jsnb/local-api";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -8,17 +8,16 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
+  .action(async (filename = "notebook.js", options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-      serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
-      console.log(`
-      Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`);
-    } catch (error) {
-      if (error.code === "EADDRINUSE") {
+      await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
+      console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`);
+    } catch (err) {
+      if (err.code === "EADDRINUSE") {
         console.error("Port is in use. Try running on a different port.");
       } else {
-        console.log(error);
+        console.log("Heres the problem", err.message);
       }
       process.exit(1);
     }
